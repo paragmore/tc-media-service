032e827f5f35227af83016527b249988284e823a
Parag More <paragmore306@gmail.com>
2023-06-15 17:03:41 +0530

    mongoose setup

--- mongoose.config.ts
+++ mongoose.config.ts
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
 import { ConnectionOptions } from "tls";
+import { environment, isProd } from "./utils/environment";
 
 export const connectMongoDB = () => {
-    console.log('hereee')
-  if (!process.env.MONGODB_URI) {
+  const uri = isProd ? process.env.MONGODB_URI : process.env.MONGODB_URI;
+  if (!uri) {
     console.log("MONGODB_URI not present");
     return;
   }
   mongoose
-    .connect(process.env.MONGODB_URI, {
+    .connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: environment.dbName,
     } as ConnectionOptions)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error(err));
