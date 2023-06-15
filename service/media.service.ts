19a4281be7b3f1bb58a7f1e2863f083e5fda290d
Parag More <paragmore306@gmail.com>
2023-06-15 17:39:44 +0530

    return uploaded url

--- service/media.service.ts
+++ service/media.service.ts
@@ -7,9 +7,12 @@ export class MediaService {
   constructor(@inject(MediaRepo) private mediaRepo: MediaRepo) {}
 
   async uploadProductImages(files: AsyncIterableIterator<MultipartFile>) {
+    const responses = [];
     for await (const file of files) {
       console.log(file);
       const response = await this.mediaRepo.uploadFileToBucket(file);
+      responses.push(response);
     }
+    return responses;
   }
 }
