7d0222b0b393a6b58622b935e8f41c367240d55d
Parag More <paragmore306@gmail.com>
2023-06-15 18:52:22 +0530

    folder structure and progress

--- service/media.service.ts
+++ service/media.service.ts
@@ -1,16 +1,22 @@
 import { inject, injectable } from "inversify";
 import { MediaRepo } from "../repo/media.repo";
 import { MultipartFile } from "@fastify/multipart";
+import { UploadProductImagesRequestI } from "../types/types";
 
 @injectable()
 export class MediaService {
   constructor(@inject(MediaRepo) private mediaRepo: MediaRepo) {}
 
-  async uploadProductImages(files: AsyncIterableIterator<MultipartFile>) {
+  async uploadProductImages(
+    files: AsyncIterableIterator<MultipartFile>,
+    request: { isVariant: boolean; storeId: string }
+  ) {
     const responses = [];
     for await (const file of files) {
-      console.log(file);
-      const response = await this.mediaRepo.uploadFileToBucket(file);
+      const folder = request.isVariant
+        ? `products/${request.storeId}/variants`
+        : `products/${request.storeId}`;
+      const response = await this.mediaRepo.uploadFileToBucket(file, folder);
       responses.push(response);
     }
     return responses;
